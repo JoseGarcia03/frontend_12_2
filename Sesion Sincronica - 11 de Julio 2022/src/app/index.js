850cccc1dbc66dc3209b7d8a707555618813e6c8
Jose Garcia <jose.garcia@agileinnova.org>
2022-07-12 09:05:30 -0500

    webpack multi html and js

--- Sesion Sincronica - 11 de Julio 2022/src/app/index.js
+++ Sesion Sincronica - 11 de Julio 2022/src/app/index.js
@@ -1,12 +1,17 @@
-import { getMethod } from "./get.js";
+import { getMethod } from "./helpers/get.js";
+import { mostrar_personajes, title } from "./modules/mostrar_dom.js";
+// Bootstrap
+import 'bootstrap/dist/css/bootstrap.min.css';
+// import "./styles/style.css";
 
-
+const container = document.getElementById("container");
 
 document.addEventListener( "DOMContentLoaded", async() => {
 
     try {
         const personajes = await getMethod();
-        console.log(personajes);
+        document.getElementById("title").innerHTML += title();
+        mostrar_personajes( personajes, container );
     } catch ( err ) {
         console.log( err );
     }
--- Sesion Sincronica - 11 de Julio 2022/webpack.config.js
+++ Sesion Sincronica - 11 de Julio 2022/webpack.config.js
@@ -1,22 +1,57 @@
 const path = require("path");
 
-const html_webpack_plugin = require("html-webpack-plugin")
+// const nodeExternals = require("webpack-node-externals");
+const html_webpack_plugin = require("html-webpack-plugin");
+const mini_css_extract_plugin = require("mini-css-extract-plugin");
 
 module.exports = {
     mode: "none",
+    // target: "node",
+    // externals: [nodeExternals()],
     entry: {
-        app: ["@babel/polyfill", "./src/app/index.js"]
+        index: ["@babel/polyfill", "./src/app/index.js"],
+        home: ["@babel/polyfill", "./src/app/scripts/home.js"]
     },
     output: {
         path: path.resolve( __dirname, "build" ),
-        filename: "js/app.bundle.js"
+        filename: "js/[name].js"
     },
     devServer: {
         port: 5500
     },
+    module: {
+        rules: [
+            {
+                test: /\.css$/i,
+                use: [ mini_css_extract_plugin.loader, "css-loader" ]
+            },
+            {
+                test: /\.js$/i,
+                loader: "babel-loader",
+                exclude: "/node_modules/"
+            }
+        ]
+    },
     plugins: [
         new html_webpack_plugin({
-            template: "./src/index.html"
+            template: "./src/index.html",
+            chunks: [ "index" ],
+            minify: {
+                removeComments: true,
+                useShortDoctype: true
+            }
+        }),
+        new html_webpack_plugin({
+            template: "./src/pages/home.html",
+            filename: "pages/home.html",
+            chunks: [ "home" ],
+            minify: {
+                removeComments: true,
+                useShortDoctype: true
+            }
+        }),
+        new mini_css_extract_plugin({
+            filename: "css/index.css"
         })
     ]
 }
